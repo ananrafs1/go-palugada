4cb226e146dbc98bbbb3c639a7942109cd8824b1
ananrafs1 <ananrafs1@gmail.com>
2022-02-04 16:21:07 +0700

    change worker to bind with task

--- main.go
+++ main.go
@@ -3,19 +3,36 @@ package main
 import (
 	"fmt"
 	"github.com/ananrafs1/go-palugada/worker"
+	"sync"
+	"time"
+	"math/rand"
 )
 
 
 func main() {
-	o, _ := Workers{}.TugasBersama(5, printer{})
-	for range o {
-		print(o)
+	var sg sync.WaitGroup
+	for i:= 0; i < 100; i++ {
+		sg.Add(1)
+		go func(i int) {
+			defer sg.Done()
+			outch, _ := (&worker.Workers{}).TugasBersama(5, &printer{})
+			for output := range outch {
+				print(fmt.Sprintf("%s %d", output.(string), i))
+			}
+		}(i)
 	}
+	sg.Wait()
+
+	Workers := make(Workers,0)
 }
 
 
 type printer struct{}
-func (p *printer) Task(successOutput chan interface{}, errorOutpur chan error){
+func (p *printer) Task(successOutput chan interface{}, errorOutpur chan error, close func()){
+	defer close()
+	rand.Seed(time.Now().UnixNano())
+	n := 2 + rand.Intn(5-2)
+	time.Sleep(time.Duration(n)* time.Second)
 	successOutput <- "TUGAS DISELESAIKAN "
 }
 
--- worker/worker.go
+++ worker/worker.go
@@ -1,34 +1,61 @@
 package worker
 
-
+import(
+	"sync"
+	"context"
+	"fmt"
+)
 type Itask interface {
-	Task(successOutput chan interface{}, errorOutpur chan error)
+	Task(successOutput chan interface{}, errorOutpur chan error, close func())
 }
 
-type Workers struct {
-
+type Worker struct {
+	Task Itask
+	OutputChan chan interface{}
+	ErrorChan chan error
 }
 
 
 
-func (w Workers) DefineWorker(n int) []Workers{
-	wout := make([]Workers,0)
+func (w Worker) DefineWorker(n int) []Worker{
+	wout := make([]Worker,0)
 	for {
 		if n < 1 {
 			break
 		}
-		wout = append(wout, Workers{})
+		wout = append(wout, Worker{})
 		n--
 	}
 	return wout
 }
 
-func (w *Workers) TugasBersama(n int, task Itask) (<-chan interface{}, <-chan error) {
-	outch := make(chan interface{})
-	errch := make(chan error)
+func (w *Worker) TugasBersama(n int, task Itask) ( chan interface{},  chan error) {
+	outch := make(chan interface{}, n)
+	errch := make(chan error, n)
 	ws := w.DefineWorker(n)
+	var syncG sync.WaitGroup
 	for range ws {
-		go task.Task(outch, errch)
+		syncG.Add(1)
+		go task.Task(outch, errch, func() { syncG.Done() })
 	}
+	syncG.Wait()
+	close(outch)
+	close(errch)
 	return outch, errch
+}
+
+func (w *Worker) Do (ctx context.Context, close func())  {
+	go w.Task.Task(w.OutputChan, w.ErrorChan, close)
+}
+
+type Workers []Worker
+
+func (w *Workers) DoAsync (ctx context.Context) {
+	// var syncG sync.WaitGroup
+	for ws := range *w {
+		fmt.Println(ws)
+		// syncG.Add(1)
+		// (&ws).Do(ctx, func() {syncG.Done()})
+	}
+	// syncG.Wait()
 }
